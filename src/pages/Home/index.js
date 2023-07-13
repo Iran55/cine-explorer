e9b632897fdb75c3898f69677a1c79b82679929f
Ribeiro55 <Iran.ribeiro55@gmail.com>
2023-07-13 12:08:03 -0300

    conclusão da chamada da api, trazendo os filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,11 +4,12 @@ import { useState, useEffect  } from "react";
 function Home() {
 
     const [movies, setMovies] = useState([])
-
+    const image_path = 'https://image.tmdb.org/t/p/w500'
     useEffect(() =>{
 
-      fetch('https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1')
-
+      fetch(`https://api.themoviedb.org/3/movie/popular?api_key=4401a05246d2db2b249eb9f3f5eef25a&language=pt-BR&page=1`)
+      .then(response => response.json())
+      .then(data => setMovies(data.results)) 
     })
 
     return (
@@ -19,7 +20,7 @@ function Home() {
               {movies.map(movie => {
                   return (
                     <Movie key={movie.id}>
-                      <a href="https://google.com.br"><img src={movie.image_url} alt={movie.title} /></a>
+                      <a href="https://google.com.br"><img src={`${image_path}${movie.poster_path}`} alt={movie.title} /></a>
                     <span>{movie.title}</span>
                     </Movie>
                   )
